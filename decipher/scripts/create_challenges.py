ead322f4c190f3ebd0b5af03da7860708e00933f
Leandro <leandrohr@protonmail.com>
2019-01-17 11:11:17 -0200

    Add support for 'page challenges' and challenges are now obfuscated

--- decipher/scripts/create_challenges.py
+++ decipher/scripts/create_challenges.py
@@ -1,10 +1,17 @@
 import os
 import csv
+import uuid
+import shutil
 from challenge.models import Challenge
 from hashlib import sha256
 
+
 # all challenges directories
 base_path = "challenge/static/challenges_files/"
+
+# first of all, clone challenges_files directory
+# from scripts folder to static folder
+shutil.copytree("scripts/challenges_files", base_path)
 
 # settings file path
 settings_path = "scripts/settings.csv"
@@ -16,6 +23,7 @@ with open(settings_path, 'r') as settings_file:
 
     # for each challenge...
     id_challenge = 0
+    unique_key = []
     for settings in reader:
 
         # ... get challenge settings
@@ -24,11 +32,27 @@ with open(settings_path, 'r') as settings_file:
         challenge_flag  = settings[2]
         challenge_description = settings[3]
 
-        # get the content path (image, download, link or page)
-        content_name = os.listdir(base_path + challenge_title)
-        content_path = "challenges_files/" + challenge_title + "/" + content_name[0]
+        # generate unique key to ubfuscate challenge folder
+        new_key = uuid.uuid4().hex[:25]
+        while new_key in unique_key:
+            new_key = uuid.uuid4().hex[:25]
+        unique_key.append(new_key)
 
-        # and create the challenge object
+        # rename challenge folder (to ubfuscate)
+        new_title = unique_key[id_challenge]
+        os.makedirs(base_path + new_title, exist_ok=True)
+        os.rename(base_path + challenge_title, base_path + new_title)
+
+        # get the content path (image, download, link or page)
+        content_files = os.listdir(base_path + new_title)
+        if challenge_type != 'page':
+            content_path = "challenges_files/" + new_title + "/" + content_files[0]
+        else:
+            for f in content_files:
+                if f[-4:] == 'html':
+                    content_path = "challenges_files/" + new_title + "/" + f
+
+        # create the challenge object
         Challenge.objects.create(
             id_chall = id_challenge,
             type_chall = challenge_type,
