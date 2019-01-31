44bf26898e795f0b56dd0cf7f55a87772443af71
Leandro <leandrohr@protonmail.com>
2019-01-31 01:05:01 -0200

    reset password module is redirecting to login page after pass change; also, it wont't show change pass forms if token is invalid

--- decipher/challenge/urls.py
+++ decipher/challenge/urls.py
@@ -13,17 +13,31 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/', views.PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+
+    # password reset module (lost password)
     path('password_reset/',
-         auth_views.PasswordResetView.as_view(template_name='challenge/password_reset.html'),
-         name='password_reset'),
+        auth_views.PasswordResetView.as_view(
+            template_name='challenge/password_reset.html'
+        ),
+        name='password_reset'
+    ),
     path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(template_name='challenge/password_reset_done.html'),
-         name='password_reset_done'),
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='challenge/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
     path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='challenge/password_reset_confirm.html'),
-         name='password_reset_confirm'),
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='challenge/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
     path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='challenge/password_reset_complete.html'),
-         name='password_reset_complete'),
+        views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
 ]
