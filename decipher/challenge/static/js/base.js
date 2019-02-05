ea1b81778e9b4c4cf62aa408e7d95a531736e998
Leandro <leandrohr@protonmail.com>
2019-02-05 18:04:50 -0200

    move mobile navbar to the right

--- decipher/challenge/static/js/base.js
+++ decipher/challenge/static/js/base.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var options = {
+    edge: 'right'
   }
   var instances = M.Sidenav.init(elems, options);
 });
